84722db1a961992bcb45c80a919be86f4af27e6e
Josh Eimer <jeimer@Joshs-MacBook-Pro.local>
2017-01-09 22:57:18 -0600

    setting up for heroku  push

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
 					   global.DATABASE_URL ||
 					   (process.env.NODE_ENV === 'production' ?
-							'mongodb://localhost/shopping-list':
-							'mongodb://localhost/shopping-list-dev');
+							'mongodb://joshatron:bluefive63@ds161008.mlab.com:61008/shoppingcart':
+							'mongodb://joshatron:bluefive63@ds161008.mlab.com:61008/shoppingcart-dev');
 exports.PORT = (process.env.PORT || 8080);
