e79cfd1289f6ffb034b968dbcd412de11fe2a640
Josh Eimer <jeimer@Joshs-MacBook-Pro.local>
2017-01-09 22:23:17 -0600

    fixing port issue

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
 					   (process.env.NODE_ENV === 'production' ?
 							'mongodb://localhost/shopping-list':
 							'mongodb://localhost/shopping-list-dev');
-exports.PORT = 8080;
+exports.PORT = (process.env.PORT || 8080);
